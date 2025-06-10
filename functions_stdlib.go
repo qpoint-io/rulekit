79f1d5c92295e60fe1dd04175d76c424d0a8102a
Kamal Nasser <kamal@qpoint.io>
2025-06-10 18:45:59 +0300

    add index function to stdlib
    `index(container, key)`
    
    index is used to index into a map via a string key or an array via an integer index.
    
    e.g. `index(map, "key")`
    	 map: `map[string]any{"key": "value"}` -> `"value"`
    
    	 `index(["first", "second"], 1)` -> `"second"`

--- functions_stdlib.go
+++ functions_stdlib.go
@@ -26,4 +26,56 @@ var StdlibFuncs = map[string]*Function{
 			}
 		},
 	},
+
+	// index(container, key)
+	//
+	// index is used to index into a map via a string key or an array via an integer index.
+	//
+	// e.g. index(map, "key")
+	//      map: map[string]any{"key": "value"} -> "value"
+	//
+	//      index(["first", "second"], 1) -> "second"
+	"index": {
+		Args: []FunctionArg{
+			{Name: "container"},
+			{Name: "key"},
+		},
+		Eval: func(args map[string]any) Result {
+			container, err := IndexFuncArg[any](args, "container")
+			if err != nil {
+				return Result{Error: err}
+			}
+
+			switch c := container.(type) {
+			case KV:
+				key, err := IndexFuncArg[string](args, "key")
+				if err != nil {
+					return Result{Error: err}
+				}
+
+				val, ok := IndexKV(c, key)
+				if !ok {
+					return Result{Error: fmt.Errorf("key %q not found", key)}
+				}
+
+				return Result{Value: val}
+
+			case []any:
+				key, err := IndexFuncArg[int64](args, "key")
+				if err != nil {
+					return Result{Error: err}
+				}
+
+				if key < 0 || int(key) >= len(c) {
+					return Result{Error: fmt.Errorf("index %d out of bounds", key)}
+				}
+
+				return Result{
+					Value: c[key],
+				}
+			}
+
+			return Result{Error: fmt.Errorf("container must be a map or array")}
+		},
+	},
 }
